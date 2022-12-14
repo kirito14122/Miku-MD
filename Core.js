d1d0a1f29b4af0b9ce43603098b18d40ef0f7594
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2022-12-14 21:09:04 +0100

    Update Core.js

--- Core.js
+++ Core.js
@@ -1902,8 +1902,8 @@ case 'bank':  case 'levee': {
 
     let buttonspro = [
         {buttonId: `${prefix}wallet`, buttonText: {displayText: 'wallet'}, type: 1},
-        {buttonId: `${prefix}withdraw`, buttonText: {displayText: 'withdraw'}, type: 1}
-        {buttonId: `${prefix}withdraw ${balance.bank}`, buttonText: {displayText: 'withdraw ${balance.bank}'}, type: 1}
+        //{buttonId: `${prefix}withdraw`, buttonText: {displayText: 'withdraw'}, type: 1}
+        {buttonId: `${prefix}withdraw ${balance.bank}`, buttonText: {displayText: `withdraw ${balance.bank}`}, type: 1}
         ]
                 let buttonMessage = {
                     image: { url: pfp },
