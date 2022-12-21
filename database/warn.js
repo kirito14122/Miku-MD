48fdbd48d5ac0feedd50328de79b24dd8ee04459
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2022-12-21 23:05:28 +0100

    Delete warn.js

--- database/warn.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const BlockSchema = new mongoose.Schema({
-id: { type: String,required: true },
-warnedby: { type: String, default: "false" }
-})
-const warndb =mongoose.model("warndb", BlockSchema)
-module.exports = { warndb }
